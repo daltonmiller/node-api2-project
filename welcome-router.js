69811c02c5304a46bbf28fb13d5556efc8f9a3c6
Dalton Miller <daltonmiller1145@Daltons-MacBook-Pro.local>
2020-09-02 18:20:30 -0600

    deploying 2.0

--- welcome-router.js
+++ welcome-router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
     res.json({
-        message: `welcome ${process.env.COHORT}`,
+        // message: `welcome ${process.env.COHORT}`,
         fact: process.env.FUN_FACT || "i have no fun facts"
     })
 })
